c92e259cad13a3f1e7ede047b834b46295d35e43
wildbillu <111654693+wildbillu@users.noreply.github.com>
2022-10-24 14:53:17 -0600

    Add files via upload

--- docs/TC-Defines.js
+++ docs/TC-Defines.js
@@ -1,5 +1,4 @@
 var g_sCookie;
-var g_sPuzzleVersion = '&nbsp;&nbsp;&nbsp;V.0.0.020'
 var g_bPuzzleSolved = false;
 var g_bGridSolved = false;
 var g_bAnswersSolved = false;
@@ -9,7 +8,7 @@ var g_sPuzzleDate = '';
 var g_sPuzzleName = '';
 var g_sPuzzleTitle = '';
 var g_sPuzzleCreditAuthor = 'By Sketchi Bill';
-var g_sPuzzleCreditDate = 'September 26, 2022';
+var g_sPuzzleCreditDate = 'September 30, 2022';
 var g_bDisplayMessages = true;
 //
 var g_ST_sClue_Intro = 'SketchiToons Clue to Dual Answer';
@@ -18,3 +17,5 @@ var g_ST_sClue_Itself = '';
 var g_ST_sFilename = '';
 var g_ST_sFilename_Extra = '';
 var g_ST_sFilename_Success = '';
+
+var g_sPuzzleVersion = '------V.0.0.020'
--- docs/TC-Info-Content.js
+++ docs/TC-Info-Content.js
@@ -7,12 +7,13 @@ function Make_Info_Content()
     sInfoContent += '<div align=right><TABLE><TR><TD>Info</TD><TD><BUTTON class="Button_Info_CloseBox Id="Info_CloseBox" onclick="Button_Info_Click();">Close</TD></TR></TABLE></div>';
     sInfoContent += '<DIV class="Info_Center"><text Id="Info_Content_AuthorCredit" class="Info_Content_Credits Info_Content_Credits_Author">Puzzle By: Sketchi Bill - Image By: Sketchi Bill</text></DIV>';
     sInfoContent += '<DIV class="Info_Center"><text Id="Info_Content_PuzzleDate" class="Info_Content_Credits Info_Content_Credits_Date">September 27, 2022</text></DIV>';
-    sInfoContent += '<DIV class="Info_Left"><text Id="Info_Content_PuzzleVersion" class="Info_Content_Credits Info_Content_Credits_Version">October 10, 2022</text></DIV>';
+    sInfoContent += '<DIV class="Info_Left"><text Id="Info_Content_PuzzleVersion" class="Info_Content_Credits Info_Content_Credits_Version"></text></DIV>';
     sInfoContent += '</DIV>';
     return sInfoContent;
 }
 function Make_Info_Content_Update()
 {
+alert(g_sPuzzleVersion)
     document.getElementById("Info_Content_PuzzleVersion").innerHTML = g_sPuzzleVersion;
     document.getElementById("Info_Content_AuthorCredit").innerHTML = g_sPuzzleCreditAuthor;
     document.getElementById("Info_Content_PuzzleDate").innerHTML = g_sPuzzleCreditDate;
