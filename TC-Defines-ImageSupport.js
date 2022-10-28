a744ec93bc6197b3d37ca08a08bddbdeb23c005e
wildbillu <111654693+wildbillu@users.noreply.github.com>
2022-10-27 18:04:18 -0600

    Delete TC-Defines-ImageSupport.js

--- TC-Defines-ImageSupport.js
+++
@@ -1,6 +0,0 @@
-// TC-Defines-ImageSupport.js
-
-var g_sImagePath_General = 'images/';
-var g_sImagePath_GridNumber = 'GridNumbers/';
-var g_sImagePath_Letters = 'Letters/';
-var g_sLetterForCorrect = 'G';
